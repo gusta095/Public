a5ff245e49385e9b678b9e81a5822c45d467b926
gusta095 <gustavo.gsouza@outlook.com>
2020-11-04 17:36:12 -0300

    estudo

--- python/AWS/lambda-list-s3.py
+++ python/AWS/lambda-list-s3.py
@@ -1,21 +1,18 @@
 import boto3
+s3 = boto3.resource('s3')
 
 BUCKET_NAME = 'gusta-origem'
 
-def lista_todos_objetos():
-  s3 = boto3.resource('s3')
+# Lista todos os objetos do bucket
+def lambda_handler(event, context):
   my_bucket = s3.Bucket(BUCKET_NAME)
   for file in my_bucket.objects.all():
     print(file.key)
 
-lista_todos_objetos()
 
-def busca_precisa():
-  s3 = boto3.resource('s3')
+# Lista os objetos buscando por uma palavra
+def lambda_handler(event, context):
   my_bucket = s3.Bucket(BUCKET_NAME)
-  busca = str(input("palavra_chave: "))
   for file in my_bucket.objects.all():
-    if busca in file.key:
+    if "card" in file.key:
       print(file.key)
-
-
