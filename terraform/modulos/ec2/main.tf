6d5c24c0da37d3bd1411953e3d310499424ef7cf
gusta095 <gustavo.gsouza@outlook.com>
2021-06-04 10:22:46 -0300

    update ec2

--- terraform/modulos/ec2/main.tf
+++ terraform/modulos/ec2/main.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "gusta-ec2" {
   count           = var.instance_number
   ami             = var.ami
   instance_type   = var.instance_type
-  # key_name        = var.key_name
-  # subnet_id       = var.subnet_id
-  # security_groups = var.security_groups
+  key_name        = var.key_name
+  subnet_id       = var.subnet_id
+  security_groups = var.security_groups
 
   tags = {
     Name = "${var.tags}-${count.index + 1}"
