43a9ba769997a68e8fa3ae79e6ceedb712e7cefa
gusta095 <gustavo.gsouza@outlook.com>
2021-06-04 13:10:57 -0300

    update starts

--- terraform/stars/instance-simple/main.tf
+++ terraform/stars/instance-simple/main.tf
@@ -1,25 +1,22 @@
 provider "aws" {
   profile = "gusta-terraform"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
-module "security-group" {
-  source = "./modulos/security-group"
-}
+
 module "keypair" {
-  source = "./modulos/keypair"
+  source = "git::https://github.com/gusta095/module-terraform-keypair.git"
 }
 module "ec2" {
-  source = "./modulos/ec2"
+  source = "git::https://github.com/gusta095/module-terraform-ec2.git"
 
-  key_name        = "${module.keypair.key_name}"
-  security_groups = "${module.security-group.id}"
+  key_name = module.keypair.key_name
 }
 
 
 output "Instance-id" {
-  value = "${module.ec2.Instance-id}"
+  value = module.ec2.Instance-id
 }
 output "Public-IP" {
-  value = "${module.ec2.Public-IP}"
+  value = module.ec2.Public-IP
 }
