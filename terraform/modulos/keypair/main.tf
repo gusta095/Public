ef1a6397d8b06487eb77b54d1016b41518079083
gusta095 <gustavo.gsouza@outlook.com>
2021-06-04 10:16:52 -0300

    update keypair

--- terraform/modulos/keypair/main.tf
+++ terraform/modulos/keypair/main.tf
@@ -4,23 +4,25 @@ locals {
 }
 
 resource "tls_private_key" "default" {
-  algorithm = "${var.ssh_key_algorithm}"
+  algorithm = var.ssh_key_algorithm
 }
 
 resource "aws_key_pair" "generated" {
-  depends_on = ["tls_private_key.default"]
   key_name   = "${var.client_name}-${var.environment}"
-  public_key = "${tls_private_key.default.public_key_openssh}"
+  public_key = tls_private_key.default.public_key_openssh
 }
 
 resource "local_file" "public_key_openssh" {
-  depends_on = ["tls_private_key.default"]
-  content    = "${tls_private_key.default.public_key_openssh}"
-  filename   = "${local.public_key_filename}"
+  content    = tls_private_key.default.public_key_openssh
+  filename   = local.public_key_filename
 }
 
 resource "local_file" "private_key_pem" {
-  depends_on = ["tls_private_key.default"]
-  content    = "${tls_private_key.default.private_key_pem}"
-  filename   = "${local.private_key_filename}"
+  content    = tls_private_key.default.private_key_pem
+  filename   = local.private_key_filename
+}
+
+provider "aws" {
+  profile = "gusta-terraform"
+  region  = "us-east-1"
 }
