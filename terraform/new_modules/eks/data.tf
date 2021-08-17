83a66d003a77ed8a18b62c53bfb5cce70e7955f3
gusta095 <gustavo.gsouza@outlook.com>
2021-08-17 10:51:11 -0300

    ajuste

---
+++ terraform/new_modules/eks/data.tf
@@ -0,0 +1,11 @@
+data "aws_iam_policy_document" "role_eks_data" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
--- terraform/new_modules/eks/policy.tf
+++ terraform/new_modules/eks/policy.tf
@@ -1,20 +1,29 @@
-resource "aws_iam_role" "role_eks" {
-  name = var.role_name
+# resource "aws_iam_role" "role_eks" {
+#   name = var.role_name
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+#   assume_role_policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "eks.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# POLICY
+
+#   tags = {
+#     Name = "role_eks"
+#   }
+# }
+
+resource "aws_iam_role" "role_eks" {
+  name               = var.role_name
+  assume_role_policy = data.aws_iam_policy_document.role_eks_data.json
 
   tags = {
     Name = "role_eks"
